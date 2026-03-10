6aea6ee1131ba2145c854f439ad9bd86c9e76251
Abuoma David <davidabuoma4@gmail.com>
2026-03-10 13:48:38 +0100

    Exercise 9

---
+++ Exercisenine.js
@@ -0,0 +1,15 @@
+// Exercise 9: Multiplication Table
+
+function printTable(base) {
+  for (let i =1; i <= 24; i++) {
+    console.log(`${base} x ${i} = ${base * i}`);
+  }
+}
+
+printTable(7);
+
+console.log(``);
+printTable(9);
+
+console.log(``);
+printTable(10);
