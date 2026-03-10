6a2991a16777731faf4c2b4b34d34bfa4d6aeced
Abuoma David <davidabuoma4@gmail.com>
2026-03-10 11:51:21 +0100

    Exercise 3

---
+++ Exercisethree.js
@@ -0,0 +1,10 @@
+// Exercise 3: Simple Calculator
+const numA = 48;
+const numB = 6;
+
+console.log(`Sum: ${numA + numB}`);
+console.log(`Difference: ${numA - numB}`);
+console.log(`Product: ${numA * numB}`);
+console.log(`Quotient: ${numA / numB}`);
+console.log(`Remainder: ${numA % numB}`);
+console.log(`${numA ** 2}: ${numB ** 2}`);
