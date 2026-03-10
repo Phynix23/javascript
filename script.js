8f23bc24aa9412bdb55a5ce6e1ce0834f29c6902
Abuoma David <davidabuoma4@gmail.com>
2026-03-10 10:56:07 +0100

    Init:setup

---
+++ script.js
@@ -0,0 +1,19 @@
+const todos = [
+  {
+    id: 1,
+    text: 'Take out trash',
+    isCompleted: true
+  },
+  {
+    id: 2,
+    text: 'Meeting wih Boss',
+    isCompleted: true
+  },
+  {
+    id: 3,
+    text: 'Denstist appointment',
+    isCompleted: false
+  },
+];
+
+
