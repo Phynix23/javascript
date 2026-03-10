cf1724c980ede35cb88aac74e5a4e05ce6b4b329
Abuoma David <davidabuoma4@gmail.com>
2026-03-10 11:12:51 +0100

    This is exercise 1

---
+++ Exerciseone.js
@@ -0,0 +1,11 @@
+// Exercise 1: Personal Introduction
+
+const fullName = 'Abuoma David';
+const age = 27;
+const city = 'Ajah';
+const state = 'Lagos';
+const favouriteSubject = 'Chemistry'
+
+console.log(
+  `My name is ${fullName}. I am ${age} years old, I live in ${state}, and my favourite subject is ${favouriteSubject}`
+)
