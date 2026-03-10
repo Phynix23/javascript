b5cd11642ac0eb0e4255fb4c83aa5b36914eeb4c
Abuoma David <davidabuoma4@gmail.com>
2026-03-10 12:24:50 +0100

    exercise 4

---
+++ Exercisefour.js
@@ -0,0 +1,9 @@
+// Exercise 4: Srting Manipulation
+const myFullName = 'Abuoma David Obionuoha';
+
+console.log(`${myFullName.length}`);
+console.log(`${myFullName.toUpperCase()}`);
+console.log(`${myFullName.toLowerCase()}`)
+console.log(
+  `Hello my name is ${myFullName} and it has ${myFullName.length} characters.`,
+)
--- Exercisethree.js
+++ Exercisethree.js
@@ -2,9 +2,9 @@
 const numA = 48;
 const numB = 6;
 
-console.log(`Sum: ${numA + numB}`);
-console.log(`Difference: ${numA - numB}`);
-console.log(`Product: ${numA * numB}`);
-console.log(`Quotient: ${numA / numB}`);
-console.log(`Remainder: ${numA % numB}`);
+console.log(`${numA + numB}`);
+console.log(`${numA - numB}`);
+console.log(`${numA * numB}`);
+console.log(`${numA / numB}`);
+console.log(`${numA % numB}`);
 console.log(`${numA ** 2}: ${numB ** 2}`);
