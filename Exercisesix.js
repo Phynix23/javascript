ed61ab7ed9e38472af306d0548035f627665ac13
Abuoma David <davidabuoma4@gmail.com>
2026-03-10 13:00:21 +0100

    Exercise 6

---
+++ Exercisesix.js
@@ -0,0 +1,15 @@
+// Exercise 6: Even or Odd
+
+function checkEvenOdd(number) {
+  if (number % 2 === 0) {
+    console.log(`${number} is even.`);
+  } else {
+    console.log(`${number} is odd.`);
+  }
+}
+
+checkEvenOdd(47)
+checkEvenOdd(0)
+checkEvenOdd(24)
+checkEvenOdd(13)
+checkEvenOdd(100)
